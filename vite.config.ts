7479154e12fa28243fccdd50dfc604f5ca2b9617
Taras Khmelovskyi <khmelovski@gmail.com>
2025-09-08 08:59:54 +0300

    test build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-// @ts-ignore
-import { resolve } from 'node:path'
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteReact(), tailwindcss(), svgr()],
 
   resolve: {
     alias: {
-      // @ts-ignore
       '@': resolve(__dirname, './src'),
     },
   },
